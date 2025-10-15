2a986d1efdf6577c8f07ec1f57f09ba14a78c4bf
Baptiste <batbox.longuepee@gmail.com>
2025-10-15 10:46:06 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -89,8 +89,6 @@ resource "aws_instance" "forum_api" {
 resource "aws_instance" "forum_db" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
-  key_name      = var.aws_key_pair
-  vpc_security_group_ids = [aws_security_group.forum_sg.id]
 
   user_data = <<-EOT
     #!/bin/bash
